5eb53bac36b22b7fe052a03db799ed2ab0e8de41
Sharva Parmar <esharva@gmail.com>
2024-01-14 17:35:46 -0800

    DriveTrain Working

---
+++ src/main/java/frc/robot/commands/DriveArcadeCustomized.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public class DriveArcadeCustomized extends CommandBase {
+  /** Creates a new DriveArcadeCustomized. */
+  DriveTrain driveTrain = null;
+  DoubleSupplier speed = null;
+  DoubleSupplier rotation = null;
+  XboxController xboxController = null;
+  double creepRotationLimit = 0.0;
+  double creepLimit = 0.0;
+  double fastLimit =0.0;
+  
+
+  public DriveArcadeCustomized(DriveTrain driveTrain, DoubleSupplier speed, DoubleSupplier rotation, double creepLimit, double creepRotationLimit, double fastLimit, XboxController xboxController) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveTrain = driveTrain;
+    this.speed = speed;
+    this.rotation = rotation;
+    this.creepLimit = creepLimit;
+    this.creepRotationLimit = creepRotationLimit;
+    this.fastLimit = fastLimit;
+    this.xboxController = xboxController;
+    addRequirements(driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    driveTrain.arcadeDriveCustomized(0, 0);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double speeds = speed.getAsDouble();
+    double rotations = rotation.getAsDouble();
+    double currentLimit = 0.6;
+    double currentRotationLimit = 0.3;
+    double speedInterval  = 0.08;
+    
+    if(xboxController.getRightBumper()){
+      currentLimit = fastLimit;
+      currentRotationLimit = 0.3;
+    }
+    else if(xboxController.getLeftBumper()) {
+      currentLimit = creepLimit;
+      currentRotationLimit = creepRotationLimit;
+    }
+
+    if((speeds >= -speedInterval) && (speeds <= speedInterval)) {
+      speeds= 0.0;
+    }
+
+    if((rotations >= -speedInterval) && (rotations <= speedInterval)) {
+      rotations = 0.0;
+    }
+
+    driveTrain.arcadeDriveCustomized(-speeds*currentLimit, rotations*currentRotationLimit);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.arcadeDriveCustomized(0, 0);
+    System.out.println("arcade drive ended");
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+
+
+public class DriveTrain extends SubsystemBase {
+
+  CANSparkMax rightFrontSpark = new CANSparkMax(Constants.RIGHT_FRONT_SPARK, MotorType.kBrushless);
+  CANSparkMax rightBackSpark = new CANSparkMax(Constants.RIGHT_BACK_SPARK, MotorType.kBrushless);
+  CANSparkMax leftFrontSpark = new CANSparkMax(Constants.LEFT_FRONT_SPARK, MotorType.kBrushless);
+  CANSparkMax leftBackSpark = new CANSparkMax(Constants.LEFT_BACK_SPARK, MotorType.kBrushless);
+
+  MotorControllerGroup rightMotors;
+  MotorControllerGroup leftMotors;
+  DifferentialDrive dDrive;
+
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+    rightFrontSpark.setInverted(false);
+    rightBackSpark.setInverted(false);
+    leftFrontSpark.setInverted(true);
+    leftBackSpark.setInverted(true);
+
+    rightFrontSpark.restoreFactoryDefaults();
+    rightBackSpark.restoreFactoryDefaults();
+    leftFrontSpark.restoreFactoryDefaults();
+    leftBackSpark.restoreFactoryDefaults();
+
+    rightFrontSpark.enableVoltageCompensation(12);
+    rightBackSpark.enableVoltageCompensation(12);
+    leftFrontSpark.enableVoltageCompensation(12);
+    leftBackSpark.enableVoltageCompensation(12);
+
+    rightMotors = new MotorControllerGroup(rightFrontSpark, rightBackSpark);
+    leftMotors = new MotorControllerGroup(leftFrontSpark, leftBackSpark);
+    dDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+    dDrive.arcadeDrive(0, 0);
+    arcadeDriveCustomized(0, 0);
+
+  }
+
+  public void arcadeDriveCustomized(double speed, double rotation){
+    rightFrontSpark.set(rotation-speed);
+    rightBackSpark.set(rotation-speed);
+    leftFrontSpark.set(speed+rotation);
+    leftBackSpark.set(speed+rotation);
+  }
+  public void stopMotors(){
+    rightFrontSpark.set(0);
+    rightBackSpark.set(0);
+    leftFrontSpark.set(0);
+    leftBackSpark.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
